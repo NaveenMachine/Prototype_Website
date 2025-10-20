ad56783bc047cbed65e525cef7c0d57427276dbc
Rishikesh Makineni <122326681+Rishikesh-Makineni@users.noreply.github.com>
2025-10-20 13:57:35 -0400

    Change BrowserRouter to use HashRouter
    Temporary Change to test HashRouter
    
    Change line 2 BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import NavBar from './components/NavBar.jsx';
 import Banner from './components/Banner.jsx';
 import About from './components/CallForPapers.jsx';
